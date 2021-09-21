819a81bb2d15cf5485b9930ca474dc250e2c2ca0
Jasdeep Singh <jasdeep.singh@QUALITEST.LOCAL>
2021-09-21 16:14:44 +0530

    next update

--- array.js
+++ array.js
@@ -1,27 +1,27 @@
-// let marks=Array(6)
-// let m1=new Array(1,3,4,6,3,4,)
+let marks=Array(6)
+let m1=new Array(1,3,4,6,3,4,)
 
-// console.log(m1.reduce((sum,m)=>(sum+m)))
-// let s=0;
-// console.log(m1.filter(s=>s%2==0).map(s=>s*2).reduce((sum,m)=>(sum+m)))
+console.log(m1.reduce((sum,m)=>(sum+m)))
+let s=0;
+console.log(m1.filter(s=>s%2==0).map(s=>s*2).reduce((sum,m)=>(sum+m)))
 
-// // let fun=function(a,b){
-// //     return a+b
-// // }
-// // console.log(fun("jasdeep","Singh"))
-// let a=(a,b)=>a+b
-// console.log(a(5,4))
-
-// console.log("teusday".indexOf("dhay"))
-
-
-// let person={
-//     f:"jasdeep ",
-//     l:"singh",
-//     fun : function(){return(this.f+this.l)}
-
+// let fun=function(a,b){
+//     return a+b
 // }
-// console.log(person.fun())
+// console.log(fun("jasdeep","Singh"))
+let a=(a,b)=>a+b
+console.log(a(5,4))
+
+console.log("teusday".indexOf("dhay"))
+
+
+let person={
+    f:"jasdeep ",
+    l:"singh",
+    fun : function(){return(this.f+this.l)}
+
+}
+console.log(person.fun())
 
 
 
